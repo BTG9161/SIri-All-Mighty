7d465edbb3ba8b18f352df9298ecb9a89c66d204
BTG9161 <bhupatsingh2817@gmail.com>
2026-07-21 17:12:23 +0530

    Stop tracking test files

--- test3.py
+++
@@ -1,3 +0,0 @@
-def is_even(num): return num % 2 == 0 
-def is_odd(num): return not is_even(num)
-def main(): print("Enter a number: ") num = int(input()) if is_odd(num): print(f"{num} is odd.") else: print(f"{num} is even.") if __name__ == "__main__": main()
